42a457c5458b72e69f1ef3476f4201fe4f805885
sidralam <sidralam@gamil.com>
2022-09-28 18:47:20 -0400

    simple basic project

---
+++ src/Class03B14/Class3.java
@@ -0,0 +1,11 @@
+package Class03B14;
+
+public class Class3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hey this is java class");
+
+	}
+
+}
